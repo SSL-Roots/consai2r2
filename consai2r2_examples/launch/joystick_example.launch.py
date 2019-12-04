75e90fc6b13fd0bec608c4d18d340ab27c8de13d
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2019-12-04 22:57:40 +0900

    Fix param name

--- consai2r2_examples/launch/joystick_example.launch.py
+++ consai2r2_examples/launch/joystick_example.launch.py
@@ -4,39 +4,40 @@ from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
 
     # parameter
-    param_joydev = LaunchConfiguration('joydev')
-    param_sim = LaunchConfiguration('sim') #TODO : 現在未使用 シミュレータの切り替え用
+    dev = LaunchConfiguration('dev')
+    sim = LaunchConfiguration('sim') #TODO : 現在未使用 シミュレータの切り替え用
     # TODO : consai2r2_descriptionからのパラメータ読み込み
-
-    ld = LaunchDescription([
-        DeclareLaunchArgument(
-            'joydev', default_value='/dev/input/js0',
+    
+    declare_dev_cmd = DeclareLaunchArgument(
+            'dev', default_value='/dev/input/js0',
             description='joystick device file'
-    )])
-
-    joy_node = Node(
+    )
+    
+    start_joy_node_cmd = Node(
             package='joy', node_executable='joy_node',
             output='screen',
-            parameters=[{'_dev' : param_joydev}]
+            parameters=[{'dev' : dev}]
     )
-
-    teleop_node = Node(
+    
+    start_teleop_node_cmd = Node(
             package='consai2r2_teleop', node_executable='teleop_node',
             output='screen'
     )
-
-    sender = Node(
+    
+    start_sender_cmd = Node(
             package='consai2r2_sender', node_executable='sim_sender',
             output='screen'
     )
-
-    ld.add_action(joy_node)
-    ld.add_action(teleop_node)
-    ld.add_action(sender)
+    
+    ld = LaunchDescription()
+    
+    ld.add_action(declare_dev_cmd)
+    ld.add_action(start_joy_node_cmd)
+    ld.add_action(start_teleop_node_cmd)
+    ld.add_action(start_sender_cmd)
 
     return ld
     
