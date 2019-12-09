0f5c3c35d407ef29205b15e986283ed380dc8364
Mirai Hattori <m-hattori@jsk.imi.i.u-tokyo.ac.jp>
2019-12-09 13:34:36 +0900

    reformat visibility_control.h

--- consai2r2_teleop/include/consai2r2_teleop/visibility_control.h
+++ consai2r2_teleop/include/consai2r2_teleop/visibility_control.h
@@ -26,31 +26,31 @@ extern "C" {
 //     https://gcc.gnu.org/wiki/Visibility
 
 #if defined _WIN32 || defined __CYGWIN__
-#ifdef __GNUC__
-#define COMPOSITION_EXPORT __attribute__((dllexport))
-#define COMPOSITION_IMPORT __attribute__((dllimport))
+  #ifdef __GNUC__
+    #define COMPOSITION_EXPORT __attribute__((dllexport))
+    #define COMPOSITION_IMPORT __attribute__((dllimport))
+  #else
+    #define COMPOSITION_EXPORT __declspec(dllexport)
+    #define COMPOSITION_IMPORT __declspec(dllimport)
+  #endif
+  #ifdef COMPOSITION_BUILDING_DLL
+    #define COMPOSITION_PUBLIC COMPOSITION_EXPORT
+  #else
+    #define COMPOSITION_PUBLIC COMPOSITION_IMPORT
+  #endif
+    #define COMPOSITION_PUBLIC_TYPE COMPOSITION_PUBLIC
+    #define COMPOSITION_LOCAL
 #else
-#define COMPOSITION_EXPORT __declspec(dllexport)
-#define COMPOSITION_IMPORT __declspec(dllimport)
-#endif
-#ifdef COMPOSITION_BUILDING_DLL
-#define COMPOSITION_PUBLIC COMPOSITION_EXPORT
-#else
-#define COMPOSITION_PUBLIC COMPOSITION_IMPORT
-#endif
-#define COMPOSITION_PUBLIC_TYPE COMPOSITION_PUBLIC
-#define COMPOSITION_LOCAL
-#else
-#define COMPOSITION_EXPORT __attribute__((visibility("default")))
-#define COMPOSITION_IMPORT
-#if __GNUC__ >= 4
-#define COMPOSITION_PUBLIC __attribute__((visibility("default")))
-#define COMPOSITION_LOCAL __attribute__((visibility("hidden")))
-#else
-#define COMPOSITION_PUBLIC
-#define COMPOSITION_LOCAL
-#endif
-#define COMPOSITION_PUBLIC_TYPE
+  #define COMPOSITION_EXPORT __attribute__((visibility("default")))
+  #define COMPOSITION_IMPORT
+  #if __GNUC__ >= 4
+     #define COMPOSITION_PUBLIC __attribute__((visibility("default")))
+     #define COMPOSITION_LOCAL __attribute__((visibility("hidden")))
+  #else
+     #define COMPOSITION_PUBLIC
+     #define COMPOSITION_LOCAL
+  #endif
+     #define COMPOSITION_PUBLIC_TYPE
 #endif
 
 #ifdef __cplusplus
