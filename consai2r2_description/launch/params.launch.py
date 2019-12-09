1b2ae1203d1ab490d0a75607e49b3487e002dd10
Mirai Hattori <m-hattori@jsk.imi.i.u-tokyo.ac.jp>
2019-12-09 18:28:50 +0900

    pass colcon test

--- consai2r2_description/launch/params.launch.py
+++ consai2r2_description/launch/params.launch.py
@@ -1,3 +1,23 @@
+# Copyright (c) 2019 SSL-Roots
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import os
 
 from launch import LaunchDescription
@@ -6,17 +26,16 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    
+
     config_path = os.path.join(
-        get_package_share_directory( 'consai2r2_description' ),
+        get_package_share_directory('consai2r2_description'),
         'config', 'config.yaml'
     )
 
     ld = LaunchDescription([
         Node(
-            package='consai2r2_description', node_executable='consai2r2_description_node', output='screen',
-            parameters=[config_path,])
+            package='consai2r2_description', node_executable='consai2r2_description_node',
+            output='screen', parameters=[config_path, ])
     ])
 
     return ld
-    
