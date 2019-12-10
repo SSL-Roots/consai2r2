17283cc41a36c46936777f616ca05f5da87ebaa6
HansRobo <pythagora.yoshimoto@gmail.com>
2019-12-10 11:31:28 +0900

    Fix consai2r2_description for colcon test

--- consai2r2_description/launch/config.launch.py
+++ consai2r2_description/launch/config.launch.py
@@ -26,17 +26,11 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    
     config_path = os.path.join(
-        get_package_share_directory( 'consai2r2_description' ),
-        'config', 'config.yaml'
-    )
+        get_package_share_directory('consai2r2_description'), 'config', 'config.yaml')
 
-    ld = LaunchDescription([
+    return LaunchDescription([
         Node(
-            package='consai2r2_description', node_executable='consai2r2_description_node', output='screen',
-            parameters=[config_path,])
+            package='consai2r2_description', node_executable='consai2r2_description_node',
+            output='screen', parameters=[config_path])
     ])
-
-    return ld
-    
